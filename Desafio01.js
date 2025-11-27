ec6c5e3ce5cd46a7d74c3345144cc3cbb514a6be
Tito <jailsonstssena@gmail.com>
2025-11-27 17:29:58 -0300

    Resolução da questão 01 do desafio

---
+++ Desafio01.js
@@ -0,0 +1,94 @@
+const dadosVendas = {
+    "vendas" : [
+        { "vendedor": "João Silva", "valor": 1200.50 },
+        { "vendedor": "João Silva", "valor": 950.75 },
+        { "vendedor": "João Silva", "valor": 1800.00 },
+        { "vendedor": "João Silva", "valor": 1400.30 },
+        { "vendedor": "João Silva", "valor": 1100.90 },
+        { "vendedor": "João Silva", "valor": 1550.00 },
+        { "vendedor": "João Silva", "valor": 1700.80 },
+        { "vendedor": "João Silva", "valor": 250.30 },
+        { "vendedor": "João Silva", "valor": 480.75 },
+        { "vendedor": "João Silva", "valor": 320.40 },
+        
+        { "vendedor": "Maria Souza", "valor": 2100.40 },
+        { "vendedor": "Maria Souza", "valor": 1350.60 },
+        { "vendedor": "Maria Souza", "valor": 950.20 },
+        { "vendedor": "Maria Souza", "valor": 1600.75 },
+        { "vendedor": "Maria Souza", "valor": 1750.00 },
+        { "vendedor": "Maria Souza", "valor": 1450.90 },
+        { "vendedor": "Maria Souza", "valor": 400.50 },
+        { "vendedor": "Maria Souza", "valor": 180.20 },
+        { "vendedor": "Maria Souza", "valor": 90.75 },
+        
+        { "vendedor": "Carlos Oliveira", "valor": 800.50 },
+        { "vendedor": "Carlos Oliveira", "valor": 1200.00 },
+        { "vendedor": "Carlos Oliveira", "valor": 1950.30 },
+        { "vendedor": "Carlos Oliveira", "valor": 1750.80 },
+        { "vendedor": "Carlos Oliveira", "valor": 1300.60 },
+        { "vendedor": "Carlos Oliveira", "valor": 300.40 },
+        { "vendedor": "Carlos Oliveira", "valor": 500.00 },
+        { "vendedor": "Carlos Oliveira", "valor": 125.75 },
+
+        { "vendedor": "Jailson Sena", "valor": 125.75 },
+        
+        { "vendedor": "Ana Lima", "valor": 1000.00 },
+        { "vendedor": "Ana Lima", "valor": 1100.50 },
+        { "vendedor": "Ana Lima", "valor": 1250.75 },
+        { "vendedor": "Ana Lima", "valor": 1400.20 },
+        { "vendedor": "Ana Lima", "valor": 1550.90 },
+        { "vendedor": "Ana Lima", "valor": 1650.00 },
+        { "vendedor": "Ana Lima", "valor": 75.30 },
+        { "vendedor": "Ana Lima", "valor": 420.90 },
+        { "vendedor": "Ana Lima", "valor": 315.40 }
+        
+        ]
+    }
+
+const totalvendas = {};
+
+function calcularComissao(valor){
+    let porcentagem = 0;
+
+    if (valor<=100){
+        porcentagem = 0;
+    }else if(valor<=500){
+        porcentagem = 0.01;
+    }else {
+        porcentagem = 0.05;
+    }
+
+    return Math.round((valor*porcentagem)*100)/100;
+}
+
+for (const vendas of dadosVendas.vendas){
+    const nomeVendedor = vendas.vendedor;
+    const valorVendas = vendas.valor;
+    const totalComissao = calcularComissao(valorVendas);
+
+    if (!totalvendas[nomeVendedor]) {
+        totalvendas[nomeVendedor]={
+        totalvendas: 0,
+        totalComissao: 0
+    };
+}
+
+totalvendas[nomeVendedor].totalvendas += valorVendas;
+totalvendas[nomeVendedor].totalComissao += totalComissao;
+}
+
+console.log("Total de Vendas e Comissão:")
+console.log("-------------------------------------")
+
+for(const vendedor in totalvendas){
+    const dados = totalvendas[vendedor];
+
+    const vendasReal = dados.totalvendas.toFixed(2);
+    const comissaoReal = dados.totalComissao.toFixed(2);
+
+    console.log(`Vendedor: ${vendedor}`);
+    console.log(`Vendas Totais: ${vendasReal}`);
+    console.log(`Total Comissão: ${comissaoReal}`);
+    console.log("------------------------------------")
+
+}
